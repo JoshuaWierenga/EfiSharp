bf5ec4cda5c6c9b1ef16d733c3bc3a8f0e49b511
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2020-10-27 22:39:47 +1100

    Fixed vhdx generation
    No longer fails to generate if an old copy exists and also fixed permissions so that the new copy will be accepted by hyperv without readding the drive.

--- EFISharp.CoreLib/EFISharp/UefiApplication.cs
+++ EFISharp.CoreLib/EFISharp/UefiApplication.cs
@@ -4,13 +4,16 @@ using EFISharp;
 
 public unsafe static class EfiApplication
 {
-    //TODO Remove function
-    [RuntimeExport("main")]
+    //TODO Remove function, so far this is required for exe outputtype,
+    //dropping that prevents link for finding EfiMain
     public static void Main()
     {
 
     }
 
+    //TODO Find way to call into other projects that this library does not depend on,
+    //this would allow matching c#'s regular main method and allow booting a library
+    //without it having a version of this function
     [RuntimeExport("EfiMain")]
     public static long EfiMain(IntPtr imageHandle, EFI_SYSTEM_TABLE* systemTable)
     {
--- EFISharp.CoreLib/System/Object.cs
+++ EFISharp.CoreLib/System/Object.cs
@@ -2,6 +2,7 @@ namespace System
 {
     public class Object
     {
+        //TODO make private or internal
         // The layout of object is a contract with the compiler.
         public IntPtr m_pEEType;
     }
