0ed18ebfff82f055d141788b3fe82889a68d0ae5
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2020-10-30 01:24:30 +1100

    Added more primitive printing support
    Also added function to read whole line.

--- EFISharp.CoreLib/EFISharp/UefiApplication.cs
+++ EFISharp.CoreLib/EFISharp/UefiApplication.cs
@@ -2,7 +2,7 @@ using System;
 using System.Runtime;
 using EFISharp;
 
-public static unsafe class EfiApplication
+public static unsafe class UefiApplication
 {
     private static EFI_SYSTEM_TABLE* SystemTable;
 
@@ -20,71 +20,328 @@ public static unsafe class EfiApplication
     public static long EfiMain(IntPtr imageHandle, EFI_SYSTEM_TABLE* systemTable)
     {
         SystemTable = systemTable;
+        Console.In = SystemTable->ConIn;
+        Console.Out = SystemTable->ConOut;
 
-        WriteLine("Hello world!");
-        WriteLine(sbyte.MinValue);
-        WriteLine(sbyte.MaxValue);
-        WriteLine(short.MinValue);
-        WriteLine(short.MaxValue);
-        WriteLine(int.MinValue);
-        WriteLine(int.MaxValue);
-        WriteLine(long.MinValue);
-        WriteLine(long.MaxValue);
-        Write('\n');
-        WriteLine(byte.MinValue);
-        WriteLine(byte.MaxValue);
-        WriteLine(ushort.MinValue);
-        WriteLine(ushort.MaxValue);
-        WriteLine(uint.MinValue);
-        WriteLine(uint.MaxValue);
-        WriteLine(ulong.MinValue);
-        WriteLine(ulong.MaxValue);
+        Console.WriteLine("string Output Test");
 
-        char input = Read();
-        Write("You entered: ");
-        WriteLine(input);
+        Console.Write('c');
+        Console.Write('h');
+        Console.Write('a');
+        Console.Write('r');
+        Console.WriteLine(" Output Test");
+
+        char* test = stackalloc char[5];
+        test[0] = 't';
+        test[1] = 'e';
+        test[2] = 's';
+        test[3] = 't';
+        test[4] = '\0';
+        Console.Write("char* Output Test: ");
+        Console.WriteLine(test);
+        Console.Write("char* Range Output Test: ");
+        Console.WriteLine(test, 1, 2);
+
+
+        Console.WriteLine("New Line Output Test");
+        Console.WriteLine();
+
+        Console.Write("sbyte Output Test: Minimum: ");
+        Console.Write(sbyte.MinValue);
+        Console.Write(", Maximum: ");
+        Console.WriteLine(sbyte.MaxValue);
+
+        Console.Write("short Output Test: Minimum: ");
+        Console.Write(short.MinValue);
+        Console.Write(", Maximum: ");
+        Console.WriteLine(short.MaxValue);
+
+        Console.Write("int Output Test: Minimum: ");
+        Console.Write(int.MinValue);
+        Console.Write(", Maximum: ");
+        Console.WriteLine(int.MaxValue);
+
+        Console.Write("long Output Test: Minimum: ");
+        Console.Write(long.MinValue);
+        Console.Write(", Maximum: ");
+        Console.WriteLine(long.MaxValue);
+
+        Console.Write("\nbyte Output Test: Minimum: ");
+        Console.Write(byte.MinValue);
+        Console.Write(", Maximum: ");
+        Console.WriteLine(byte.MaxValue);
+
+        Console.Write("ushort Output Test: Minimum: ");
+        Console.Write(ushort.MinValue);
+        Console.Write(", Maximum: ");
+        Console.WriteLine(ushort.MaxValue);
+
+        Console.Write("uint Output Test: Minimum: ");
+        Console.Write(uint.MinValue);
+        Console.Write(", Maximum: ");
+        Console.WriteLine(uint.MaxValue);
+
+        Console.Write("ulong Output Test: Minimum: ");
+        Console.Write(ulong.MinValue);
+        Console.Write(", Maximum: ");
+        Console.WriteLine(ulong.MaxValue);
+
+        Console.Write("\nbool Output Test: ");
+        Console.Write(false);
+        Console.Write(", ");
+        Console.WriteLine(true);
+
+        Console.WriteLine("\nInput Test:");
+        //TODO Fix array issues, currently a program with arrays fails link.exe
+        char* input = Console.ReadLine();
+        Console.Write("You entered: ");
+        for (int i = 0; input[i] != '\0'; i++)
+        {
+            Console.Write(input[i]);
+        }
 
         while (true) ;
     }
+}
 
-    public static void WriteLine(string value)
+public static unsafe class Console
+{
+    internal const int ReadBufferSize = 4096;
+
+    internal static EFI_SIMPLE_TEXT_INPUT_PROTOCOL* In;
+    internal static EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* Out;
+
+    //At least when I control the implementation it makes sense to just
+    //return a char since that what efi returns, System.Console.Read() does
+    //however return an int.
+    public static char Read()
     {
-        Write(value);
-        //TODO Add String.Concat?
-        Write("\r\n");
+        EFI_INPUT_KEY key;
+
+        //TODO Add WaitForEvent/WaitForKey
+        do
+        {
+            In->ReadKeyStroke(In, &key);
+        } while (key.UnicodeChar == default); //TODO Use ScanCode
+
+        return key.UnicodeChar;
     }
 
-    public static void WriteLine(long value)
+    //TODO Add char*/char[] to string
+    public static char* ReadLine()
     {
-        Write(value);
-        //TODO Add String.Concat?
-        Write("\r\n");
+        char currentKey;
+        char* input = stackalloc char[ReadBufferSize];
+        int charCount = 0;
+
+        do
+        {
+            currentKey = Read();
+            if (currentKey == '\r')
+            {
+                break;
+            }
+
+            //TODO Only allow specific chars, U+0020 to U+007E? https://en.wikipedia.org/wiki/Basic_Latin_(Unicode_block)
+            input[charCount] = currentKey;
+            charCount++;
+        } while (charCount < ReadBufferSize);
+
+        char* result = stackalloc char[charCount + 1];
+        for (int i = 0; i < charCount; i++)
+        {
+            result[i] = input[i];
+        }
+        result[charCount] = '\0';
+
+        return result;
     }
 
-    public static void WriteLine(ulong value)
+    public static void WriteLine()
+    {
+        char* pValue = stackalloc char[3];
+        pValue[0] = '\r';
+        pValue[1] = '\n';
+        pValue[2] = '\0';
+
+        Out->OutputString(Out, pValue);
+    }
+
+    public static void WriteLine(bool value)
     {
         Write(value);
-        //TODO Add String.Concat?
-        Write("\r\n");
+        WriteLine();
     }
 
     public static void WriteLine(char value)
     {
         Write(value);
-        //TODO Add String.Concat?
-        Write("\r\n");
+        WriteLine();
     }
 
-    //TODO Remove, add console classes?
-    public static void Write(string value)
+    public static void WriteLine(char* buffer)
     {
-        fixed (char* pValue = value)
+        Write(buffer);
+        WriteLine();
+    }
+
+    public static void WriteLine(char* buffer, int index, int count)
+    {
+        Write(buffer, index, count);
+        WriteLine();
+    }
+
+    //TODO Add single and double WriteLine
+    /*public static void WriteLine(decimal value)
+    {
+
+    }
+
+    public static void WriteLine(double value)
+    {
+
+    }
+
+    public static void WriteLine(float value)
+    {
+
+    }*/
+
+    public static void WriteLine(int value)
+    {
+        Write(value);
+        WriteLine();
+    }
+
+    public static void WriteLine(uint value)
+    {
+        Write(value);
+        WriteLine();
+    }
+
+    public static void WriteLine(long value)
+    {
+        Write(value);
+        WriteLine();
+    }
+
+    public static void WriteLine(ulong value)
+    {
+        Write(value);
+        WriteLine();
+    }
+
+    /*public static void WriteLine(object? value)
+    {
+        Out.WriteLine(value);
+    }*/
+
+    public static void WriteLine(string value)
+    {
+        Write(value);
+        WriteLine();
+    }
+
+    /*public static void WriteLine(string format, object? arg0)
+    {
+    }
+
+    public static void WriteLine(string format, object? arg0, object? arg1)
+    {
+    }
+
+    public static void WriteLine(string format, object? arg0, object? arg1, object? arg2)
+    {
+    }
+
+    public static void WriteLine(string format, params object?[]? arg)
+    {
+    }
+
+    public static void Write(string format, object? arg0)
+    {
+    }
+
+    public static void Write(string format, object? arg0, object? arg1)
+    {
+    }
+
+    public static void Write(string format, object? arg0, object? arg1, object? arg2)
+    {
+    }
+
+    public static void Write(string format, params object?[]? arg)
+    {
+    }*/
+
+    public static void Write(bool value)
+    {
+        if (value)
         {
-            SystemTable->ConOut->OutputString(SystemTable->ConOut, pValue);
+            char* pValue = stackalloc char[5];
+            pValue[0] = 'T';
+            pValue[1] = 'r';
+            pValue[2] = 'u';
+            pValue[3] = 'e';
+            pValue[4] = '\0';
+
+            Out->OutputString(Out, pValue);
+        }
+        else
+        {
+            char* pValue = stackalloc char[6];
+            pValue[0] = 'F';
+            pValue[1] = 'a';
+            pValue[2] = 'l';
+            pValue[3] = 's';
+            pValue[4] = 'e';
+            pValue[5] = '\0';
+
+            Out->OutputString(Out, pValue);
         }
     }
 
-    public static void Write(long value)
+    public static void Write(char value)
+    {
+        char* pValue = stackalloc char[2];
+        pValue[0] = value;
+        pValue[1] = '\0';
+
+        Out->OutputString(Out, pValue);
+    }
+
+    public static void Write(char* buffer)
+    {
+        Out->OutputString(Out, buffer);
+    }
+
+    public static void Write(char* buffer, int index, int count)
+    {
+        char* pBuffer = stackalloc char[count + 1];
+
+        for (int i = 0, j = index; i < count && buffer[j] != '\0'; i++, j++)
+        {
+            pBuffer[i] = buffer[j];
+        }
+
+        pBuffer[count] = '\0';
+        Out->OutputString(Out, pBuffer);
+    }
+
+    //TODO Add single and double Write
+    /*public static void Write(decimal value)
+    {
+    }
+
+    public static void Write(double value)
+    {
+    }
+
+    public static void Write(float value)
+    {
+    }*/
+
+    public static void Write(int value)
     {
         if (value < 0)
         {
@@ -93,21 +350,21 @@ public static unsafe class EfiApplication
             value = -value;
         }
 
-        Write((ulong)value);
+        Write((uint)value);
     }
 
-    //TODO Rewrite to make a single pointer array for char of max long length and use a single loop
-    public static void Write(ulong value)
+    //TODO Rewrite to make a single pointer array for char of max uint length and use a single loop
+    public static void Write(uint value)
     {
         //TODO Figure out why new fails at runtime
-        byte* digits = stackalloc byte[19];
+        byte* digits = stackalloc byte[10];
         byte digitCount = 0;
-        byte digitPosition = 18; //This is designed to wrap round for numbers with 19 digits
+        byte digitPosition = 9; //This is designed to wrap round for numbers with 10 digits
 
         //From https://stackoverflow.com/a/4808815
         do
         {
-            digits[digitPosition] = (byte) (value % 10);
+            digits[digitPosition] = (byte)(value % 10);
             value = value / 10;
             digitCount++;
             digitPosition--;
@@ -125,32 +382,62 @@ public static unsafe class EfiApplication
             pValue[i] = (char)(digits[digitPosition] + '0');
         }
 
-        SystemTable->ConOut->OutputString(SystemTable->ConOut, pValue);
+        Out->OutputString(Out, pValue);
     }
 
-    public static void Write(char value)
+    public static void Write(long value)
     {
-        char* pValue = stackalloc char[2];
-        pValue[0] = value;
-        pValue[1] = '\0';
+        if (value < 0)
+        {
+            Write('-');
+            //TODO Add Math.Abs?
+            value = -value;
+        }
 
-        SystemTable->ConOut->OutputString(SystemTable->ConOut, pValue);
+        Write((ulong)value);
     }
 
-
-    //At least when I control the implementation it makes sense to just
-    //return a char since that what efi returns, System.Console.Read() does
-    //however return an int.
-    public static char Read()
+    //TODO Rewrite to make a single pointer array for char of max ulong length and use a single loop
+    public static void Write(ulong value)
     {
-        EFI_INPUT_KEY key;
+        //TODO Figure out why new fails at runtime
+        byte* digits = stackalloc byte[19];
+        byte digitCount = 0;
+        byte digitPosition = 18; //This is designed to wrap round for numbers with 19 digits
 
-        //TODO Add WaitForEvent/WaitForKey
+        //From https://stackoverflow.com/a/4808815
         do
         {
-            SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &key);
-        } while (key.UnicodeChar == default);
+            digits[digitPosition] = (byte)(value % 10);
+            value = value / 10;
+            digitCount++;
+            digitPosition--;
+        } while (value > 0);
 
-        return key.UnicodeChar;
+
+        byte charCount = (byte)(digitCount + 1);
+
+        char* pValue = stackalloc char[charCount];
+        pValue[charCount - 1] = '\0';
+
+        digitPosition++;
+        for (int i = 0; i < digitCount; i++, digitPosition++)
+        {
+            pValue[i] = (char)(digits[digitPosition] + '0');
+        }
+
+        Out->OutputString(Out, pValue);
+    }
+
+    /*public static void Write(object? value)
+    {
+    }*/
+
+    public static void Write(string value)
+    {
+        fixed (char* pValue = value)
+        {
+            Out->OutputString(Out, pValue);
+        }
     }
 }
