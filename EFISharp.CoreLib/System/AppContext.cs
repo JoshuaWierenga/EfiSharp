d5a71847b851be4d929fd1015d5bd163b5a964cc
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2021-06-13 15:16:49 +1000

    Quick fix to get project building again.

---
+++ EFISharp.CoreLib/System/AppContext.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// Changes made by Joshua Wierenga.
+
+namespace System
+{
+    public static partial class AppContext
+    {
+        public static void SetData(string name, object? data)
+        {
+            //TODO Actually support, is this required any time soon though since
+            //this was only added to appease the compiler. 
+            throw new NotImplementedException(SR.Arg_NotImplementedException);
+        }
+    }
+}
